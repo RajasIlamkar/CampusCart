a19464eddd685e0fc12cdd6f510f41a30fc31c73
Rajas Ilamkar <113300283+RajasIlamkar@users.noreply.github.com>
2025-07-31 19:48:11 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,7 +10,7 @@ export default function Login({ onLogin }) {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const res = await fetch('http://localhost:5000/api/auth/login', {
+    const res = await fetch(`${process.env.REACT_APP_URL}/api/auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
